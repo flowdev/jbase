f0919228d846d49af1e7e60bc0bf1ad1480478b7
ole108 <ole@bulbuk.de>
2014-07-16 12:40:46 +0200

    Making it all a bit more Java like: Fourth step, flowparser.

--- src/main/java/org/flowdev/base/data/PrettyPrinter.java
+++ src/main/java/org/flowdev/base/data/PrettyPrinter.java
@@ -1,7 +1,9 @@
 package org.flowdev.base.data;
 
-import java.lang.reflect.Field;
-import java.util.Arrays;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 
@@ -45,34 +47,54 @@ public final class PrettyPrinter {
             buf.append(obj.getClass().getSimpleName()).append(" {").append(NL);
 
             prettyPrintEntries(indentation + INDENT, buf,
-                    fieldsToEntries(obj.getClass().getFields(), obj));
+                    methodsToEntries(obj.getClass().getMethods(), obj));
 
             buf.append(indentation).append("}");
         }
         return buf;
     }
 
-    private static Entry[] fieldsToEntries(Field[] fields, Object obj) {
-        Entry[] entries = new Entry[fields.length];
+    private static List<Entry> methodsToEntries(Method[] methods, Object obj) {
+        List<Entry> entries = new ArrayList<>(methods.length);
 
-        for (int i = 0; i < fields.length; i++) {
-            Field field = fields[i];
-            entries[i] = new Entry(field.getName(), getValueOfField(field, obj));
+        for (Method method : methods) {
+            if (isGetter(method)) {
+                entries.add(new Entry(getEntryName(method), getValueOfGetter(method, obj)));
+            }
         }
 
         return entries;
     }
 
-    private static Object getValueOfField(Field field, Object obj) {
+    private static boolean isGetter(Method method) {
+        String name = method.getName();
+        return method.getParameterCount() <= 0 && ((name.startsWith("get") && !name.equals("getClass")) || name.startsWith("is"));
+    }
+
+    private static String getEntryName(Method getter) {
+        String name = getter.getName();
+        if (name.startsWith("get")) {
+            return cutPrefix(name, 3);
+        } else {
+            return cutPrefix(name, 2);
+        }
+    }
+
+    private static String cutPrefix(String name, int prefixLen) {
+        String subName = name.substring(prefixLen);
+        return subName.substring(0, 1).toLowerCase() + subName.substring(1);
+    }
+
+    private static Object getValueOfGetter(Method method, Object obj) {
         try {
-            return field.get(obj);
-        } catch (IllegalAccessException e) {
+            return method.invoke(obj);
+        } catch (IllegalAccessException | InvocationTargetException e) {
             throw new RuntimeException(e);
         }
     }
 
-    public static StringBuilder prettyPrintEntries(String indentation, StringBuilder buf, Entry[] entries) {
-        Arrays.sort(entries);
+    public static StringBuilder prettyPrintEntries(String indentation, StringBuilder buf, List<Entry> entries) {
+        Collections.sort(entries);
 
         for (Entry entry : entries) {
             prettyPrintEntry(indentation, buf, entry.name, " : ", entry.value);
@@ -97,8 +119,7 @@ public final class PrettyPrinter {
         } else if (value instanceof List) {
             prettyPrintList(indentation, buf, (List<?>) value);
         } else if (value.getClass().isArray()) {
-            buf.append("WARNING: Arrays are not supported: ").append(
-                    value.toString());
+            prettyPrintArray(indentation, buf, (Object[]) value);
         } else {
             switch (type) {
                 case "java.lang.Boolean":
@@ -133,13 +154,11 @@ public final class PrettyPrinter {
         return buf;
     }
 
-    private static Entry[] mapToEntries(Map<?, ?> map) {
-        Entry[] entries = new Entry[map.size()];
-        int i = 0;
+    private static List<Entry> mapToEntries(Map<?, ?> map) {
+        List<Entry> entries = new ArrayList<>(map.size());
 
         for (Object key : map.keySet()) {
-            entries[i] = new Entry(escapeString(key.toString()), map.get(key));
-            i++;
+            entries.add(new Entry(escapeString(key.toString()), map.get(key)));
         }
 
         return entries;
@@ -147,9 +166,10 @@ public final class PrettyPrinter {
 
     public static String escapeString(String s) {
         return "\""
-                + s.replace("\\", "\\\\").replace("\t", "\\t")
-                .replace("\n", "\\n").replace("\r", "\\r")
-                .replace("\"", "\\\"") + "\"";
+                + s.replace("\\", "\\\\").replace("\t", "\\t").replace("\"", "\\\"")
+                .replace("\r", "\\r").replace("\n", "\\n")
+                .replace("\f", "\\f").replace("\b", "\\b")
+                + "\"";
     }
 
     public static StringBuilder prettyPrintList(String indentation, StringBuilder buf, List<?> list) {
@@ -160,6 +180,20 @@ public final class PrettyPrinter {
 
         for (int i = 0; i < N; i++) {
             prettyPrintEntry(innerIndentation, buf, "" + i, " : ", list.get(i));
+        }
+
+        buf.append(indentation).append("]");
+        return buf;
+    }
+
+    public static StringBuilder prettyPrintArray(String indentation, StringBuilder buf, Object[] array) {
+        String innerIndentation = indentation + INDENT;
+        int N = array.length;
+
+        buf.append("Array [").append(NL);
+
+        for (int i = 0; i < N; i++) {
+            prettyPrintEntry(innerIndentation, buf, "" + i, " : ", array[i]);
         }
 
         buf.append(indentation).append("]");
---
+++ src/test/java/org/flowdev/base/data/PrettyPrinterTest.java
@@ -0,0 +1,94 @@
+package org.flowdev.base.data;
+
+import org.junit.Test;
+
+import static org.flowdev.base.data.PrettyPrinter.escapeString;
+import static org.flowdev.base.data.PrettyPrinter.prettyPrint;
+import static org.junit.Assert.assertEquals;
+
+public class PrettyPrinterTest {
+
+    @Test
+    public void testPrettyPrint() throws Exception {
+        TestData data = new TestData();
+        data.setBla(true);
+        data.setBlue("Test!");
+        data.setI(123);
+
+        assertEquals("Objects aren't printed correctly.", "TestData {\n" +
+                "    bla : true ;\n" +
+                "    blue : \"Test!\" ;\n" +
+                "    i : 123 ;\n" +
+                "}\n", prettyPrint(data));
+    }
+
+    @Test
+    public void testPrettyPrintEntries() throws Exception {
+
+    }
+
+    @Test
+    public void testPrettyPrintEntry() throws Exception {
+
+    }
+
+    @Test
+    public void testPrettyPrintMap() throws Exception {
+
+    }
+
+    @Test
+    public void testEscapeString() throws Exception {
+        assertEquals("'\\' not escaped properly.", "\"\\\\\"", escapeString("\\"));
+        assertEquals("'\\' not escaped properly.", "\"a]\\\\adf=af+af\\\\sfd'\"", escapeString("a]\\adf=af+af\\sfd'"));
+        assertEquals("'\"' not escaped properly.", "\"\\\"\"", escapeString("\""));
+        assertEquals("'\"' not escaped properly.", "\"as\\\"df\\\";;:\"", escapeString("as\"df\";;:"));
+        assertEquals("Tabulator not escaped properly.", "\"\\t\"", escapeString("\t"));
+        assertEquals("Tabulator not escaped properly.", "\"12\\t34\\t]]-\"", escapeString("12\t34\t]]-"));
+        assertEquals("Newline not escaped properly.", "\"\\n\"", escapeString("\n"));
+        assertEquals("Newline not escaped properly.", "\"12\\n34\\n]]-\"", escapeString("12\n34\n]]-"));
+        assertEquals("Carriage Return not escaped properly.", "\"\\r\"", escapeString("\r"));
+        assertEquals("Carriage Return not escaped properly.", "\"12\\r34\\r]]-\"", escapeString("12\r34\r]]-"));
+        assertEquals("CR LF not escaped properly.", "\"\\r\\n\"", escapeString("\r\n"));
+        assertEquals("CR LF not escaped properly.", "\"1\\r\\n3\\r\\n]-\"", escapeString("1\r\n3\r\n]-"));
+        assertEquals("Formfeed not escaped properly.", "\"\\f\"", escapeString("\f"));
+        assertEquals("Formfeed not escaped properly.", "\"12\\f34\\f]]-\"", escapeString("12\f34\f]]-"));
+        assertEquals("Backspace not escaped properly.", "\"\\b\"", escapeString("\b"));
+        assertEquals("Backspace not escaped properly.", "\"12\\b34\\b]]-\"", escapeString("12\b34\b]]-"));
+    }
+
+    @Test
+    public void testPrettyPrintList() throws Exception {
+
+    }
+
+    public static class TestData {
+        private boolean bla;
+        private String blue;
+        private int i;
+
+        public boolean isBla() {
+            return bla;
+        }
+
+        public void setBla(boolean bla) {
+            this.bla = bla;
+        }
+
+        public String getBlue() {
+            return blue;
+        }
+
+        public void setBlue(String blue) {
+            this.blue = blue;
+        }
+
+        public int getI() {
+            return i;
+        }
+
+        public void setI(int i) {
+            this.i = i;
+        }
+    }
+}
